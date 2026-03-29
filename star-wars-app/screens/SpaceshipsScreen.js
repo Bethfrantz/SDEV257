c9fb879756a09220efc0242ae507d67e64858a3c
Bethfrantz <ehoyt@ivytech.edu>
2026-03-29 19:03:42 -0400

    starwarsapp

---
+++ star-wars-app/screens/SpaceshipsScreen.js
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native";
+
+export default function SpaceshipsScreen() {
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text>Spaceships Screen</Text>
+        </View>
+    );
+}
