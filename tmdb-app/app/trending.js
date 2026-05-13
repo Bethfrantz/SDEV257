a2aeecbb4790ca6ddc3ecfa09f79fc0d0238f2d8
Bethfrantz <ehoyt@ivytech.edu>
2026-05-13 16:59:46 -0400

    updates

--- tmdb-app/app/trending.js
+++ tmdb-app/app/trending.js
@@ -5,6 +5,7 @@ import {
   ScrollView,
   Text,
   TouchableOpacity,
+  View,
 } from "react-native";
 import { fetchTrending, IMG_URL } from "../lib/tmdb";
 
@@ -17,19 +18,42 @@ export default function Trending() {
 
   return (
     <ScrollView style={{ flex: 1, backgroundColor: "#111", padding: 10 }}>
-      {movies.map((movie) => (
-        <TouchableOpacity
-          key={movie.id}
-          onPress={() => router.push(`/movie/${movie.id}`)}
-          style={{ marginBottom: 20 }}
-        >
-          <ImageBackground
-            source={{ uri: IMG_URL + movie.poster_path }}
-            style={{ width: "100%", height: 300, borderRadius: 8 }}
-          />
-          <Text style={{ color: "white", marginTop: 10 }}>{movie.title}</Text>
-        </TouchableOpacity>
-      ))}
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {movies.map((movie) => (
+          <TouchableOpacity
+            key={movie.id}
+            onPress={() => router.push(`/movie/${movie.id}`)}
+            style={{ marginBottom: 20 }}
+          >
+            <ImageBackground
+              source={{ uri: IMG_URL + movie.poster_path }}
+              style={{
+                width: 180,
+                height: 270, // perfect TMDB poster ratio
+                borderRadius: 8,
+                overflow: "hidden",
+              }}
+            />
+
+            <Text
+              style={{
+                color: "white",
+                marginTop: 10,
+                width: 180,
+              }}
+              numberOfLines={1}
+            >
+              {movie.title}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
     </ScrollView>
   );
 }
